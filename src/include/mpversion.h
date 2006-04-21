a314b68ebddd6eefdd6fd4624db1738d3d4513e1
grahamd <grahamd>
2006-04-21 03:45:51 +0000

    PythonHandlerModule was not setting up registration of the PythonFixupHandler or PythonAuthenHandler. For the latter this meant that using Require directive with PythonHandlerModule would cause a 500 error and complaint in error log about "No groups file". (MODPYTHON-166)

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,6 +1,6 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 3
 #define MPV_PATCH 0
-#define MPV_BUILD 20060420
-#define MPV_STRING "3.3.0-dev-20060420"
+#define MPV_BUILD 20060421
+#define MPV_STRING "3.3.0-dev-20060421"
 
