6d4a1195e8240c53d524950daa7646411136e354
grahamd <grahamd>
2006-07-30 04:22:17 +0000

    MODPYTHON-38) Fixed issue when using PSP pages in conjunction with publisher handler or where a PSP error page was being triggered, that form parameters coming from content of a POST request weren't available or only available using a workaround. Specifically, the PSP page will now use any FieldStorage object instance cached as req.form left there by preceding code.

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,6 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 3
 #define MPV_PATCH 0
-#define MPV_BUILD 20060709
-#define MPV_STRING "3.3.0-dev-20060709"
+#define MPV_BUILD 20060730
+#define MPV_STRING "3.3.0-dev-20060730"
+
